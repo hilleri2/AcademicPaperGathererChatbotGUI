271564fd38dd972fb91cbd7e18ee7b93dad6a509
Autumn Clark <clarautu@isu.edu>
2025-03-07 10:53:36 -0700

    Changed file passing to stay in memory during filtering process

--- FileGatherer.py
+++ FileGatherer.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 import time
+import io
 
 import FileFilterer
 import FileWriter
@@ -10,12 +12,14 @@ class FileGatherer:
 
     # Gather files from each result, including ones that are referenced on each web page
         # @param results : List of scraped results from Google Scholar
-        # @param q : The Google Scholar search query
-    def gather_files(self, results, q):
+        # @param query : The Google Scholar search query
+    def gather_files(self, results, query):
         result_index = -1
         for result in results:  # Iterate over results
             result_index += 1
-            time.sleep(3)  # Sleep for 3 seconds to avoid angering any anti-bot policies
+            # Sleep for 3 to 7 seconds to avoid angering any anti-bot policies
+            delay = random.uniform(3, 7)
+            time.sleep(delay)
             # Some results have a file_link, which may be different from the regular link, but not all have this
             #   so, if this value is None, then just use the regular file link, which all results have
             url = result['file_link']
@@ -29,7 +33,7 @@ class FileGatherer:
             except Exception as e:
                 print("Error on index " + str(result_index) + "\n\tException: ", e)
                 continue
-            if url[-4:] == ".pdf":
+            if url[-3:] == "pdf":
                 FileWriter.FileWriter().write_file("Files\\pdf" + str(result_index) + ".pdf", response.content, 'wb')
                 print(f"Saved PDF directly from index {result_index}.")
                 continue
@@ -54,15 +58,36 @@ class FileGatherer:
                         print("\tException: ", e)
                         continue
 
-                    # Write content in pdf file
-                    file_path = "Files\\pdf" + str(result_index) + "-" + str(i) + ".pdf"
-                    FileWriter.FileWriter().write_file(file_path, response.content, 'wb')
-                    if FileFilterer.FileFilterer().filter(file_path, q):
+                    pdf_file_obj = io.BytesIO(response.content)  # Save file in memory
+                    filter_result = FileFilterer.FileFilterer().filter(pdf_file_obj, query)
+                    if filter_result[0]:
+                        file_path = "Files\\pdf" + str(result_index) + "-" + str(i) + ".pdf"
+                        writer = FileWriter.FileWriter()
+                        writer.write_file(file_path, response.content, 'wb')
+                        writer.write_file("Files\\title" + str(result_index) + "-" + str(i) + ".txt", filter_result[1], 'w')
+                        writer.write_file("Files\\keywords" + str(result_index) + "-" + str(i) + ".txt", filter_result[2], 'w')
+                        writer.write_file("Files\\author" + str(result_index) + "-" + str(i) + ".txt", filter_result[3], 'w')
+                        writer.write_file("Files\\modDate" + str(result_index) + "-" + str(i) + ".txt", filter_result[4], 'w')
                         print(f"File '{file_path}' downloaded.")
                     else:
-                        # File is bad and must be deleted
-                        FileWriter.FileWriter().remove_file(file_path)
-                        print(f"File '{file_path}' deleted.")
+                        print(f"File '{result_index}-{i}' filtered out.")
+                        if filter_result[1] is not None:
+                            writer = FileWriter.FileWriter()
+                            writer.write_file("Files\\Bad\\title" + str(result_index) + "-" + str(i) + ".txt",
+                                              filter_result[1], 'w')
+                            writer.write_file("Files\\Bad\\keywords" + str(result_index) + "-" + str(i) + ".txt",
+                                              filter_result[2], 'w')
+
+                    # Write content in pdf file
+                    # file_path = "Files\\pdf" + str(result_index) + "-" + str(i) + ".pdf"
+                    # FileWriter.FileWriter().write_file(file_path, response.content, 'wb')
+                    # result = FileFilterer.FileFilterer().filter(file_path, query)
+                    # if result[0]:
+                    #     print(f"File '{file_path}' downloaded.")
+                    # else:
+                    #     # File is bad and must be deleted
+                    #     FileWriter.FileWriter().remove_file(file_path)
+                    #     print(f"File '{file_path}' deleted.")
             if i == 0:
                 print("No files from index " + str(result_index))
         print("All results scraped.")
